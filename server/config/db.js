7550c6923723a695e1b73d3a3539eb5755351a30
Ilyas Belfar <61812764+ilyasbelfar@users.noreply.github.com>
2023-02-23 20:42:40 +0000

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
+        await mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.MONGODB_URL_CONNECTION, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
